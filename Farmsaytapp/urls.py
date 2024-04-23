f8bb51cf09811ad4fd87fc9bc6322e049d2cb959
TimurRad777 <timur.radjabov.777@gmail.com>
2024-04-23 17:22:27 +0500

    design

--- Farmsaytapp/urls.py
+++ Farmsaytapp/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('mission/', views.mission, name = 'mission'),
+    path('plant/', views.plant, name = 'plant'),
+    path('news/', views.news, name = 'news'),
+    path('buy/', views.buy, name = 'buy'),
 ]
--- Farmsaytapp/views.py
+++ Farmsaytapp/views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+def mission(request):
+    return render(request, "mission.html")
+
+def plant(request):
+    return render(request, "plant.html")
+
+def news(request):
+    return render(request, "news.html")
+
+def buy(request):
+    return render(request, "buy.html")
